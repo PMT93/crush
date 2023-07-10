ad3eab7f12c15035d60ff98cd5b5727121a5f5a6
PM_Thien <pmt010193@gmail.com>
2023-07-10 14:35:34 +0700

    upload

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,17 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text1: "Hề lố Hêu xênh đợp!",
+  text2: "Anh có điều này muốn hỏi em nhớ phải trả lời thật lòng nhaaa.",
+  text3: "Em yêu anh có phải không nào?",
+  text4: "Nếu em ko trả lời mà thoát ra tức là muốn làm vợ anh rùi đó nha <3",
+  text5: "Anh mơ à???",
+  text6: "Em yêu anh <3",
+  text7: "Nói lý do em thích anh đi ^^",
+  text8: "Gửi cho anh <3",
+  text9: "Vì anh đẹp troai bụng mỡ",
+  text10: "Anh biết mà ^^ Yêu em 300.000",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+    "Tối nay anh qua đón em đi chơi nha <3 Còn giờ thì chờ gì nữa mà ko inbox cho anh đi nàooo",
+  text12: "Ok <3",
 };
 
 $(document).ready(function () {
